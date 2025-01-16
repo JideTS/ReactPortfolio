3c8df46b2094930582a06014e67509a54c53d209
Shaduruk <jide@tidjani-serpos.com>
2025-01-16 09:19:57 -0500

    remove header from one pager portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-      <Header />
       <Outlet />
       <Footer />
     </>
