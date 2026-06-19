334053ef476eae463e661730ea44b1f6f920ae75
Shaduruk <jide@tidjani-serpos.com>
2026-06-19 06:24:55 +0200

    Refine hero font loading and alignment

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+import '@fontsource/montserrat/latin-500.css';
+import '@fontsource/montserrat/latin-600.css';
+import '@fontsource/montserrat/latin-700.css';
+import '@fontsource/montserrat/latin-800-italic.css';
+import '@fontsource/poppins/latin-400.css';
+import '@fontsource/poppins/latin-500.css';
+import '@fontsource/poppins/latin-600.css';
+import '@fontsource/roboto/latin-400.css';
+import '@fontsource/roboto/latin-500.css';
+
 import { router } from './routes';
 import './styles.css';
 
